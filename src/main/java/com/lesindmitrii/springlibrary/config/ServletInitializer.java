a8bb76b7a961e063ec220cc08a025e5fcc5b6f7d
Lesin DA <lesindmitrii92@gmail.com>
2023-01-09 18:39:33 +0300

    First commit

---
+++ src/main/java/com/lesindmitrii/springlibrary/config/ServletInitializer.java
@@ -0,0 +1,52 @@
+package com.lesindmitrii.springlibrary.config;
+
+import jakarta.servlet.DispatcherType;
+import jakarta.servlet.FilterRegistration;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import org.springframework.lang.NonNull;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import java.util.EnumSet;
+
+public class ServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{SpringConfig.class};
+    }
+
+    @NonNull
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+    
+    @Override
+    public void onStartup(@NonNull ServletContext servletContext) throws ServletException {
+        super.onStartup(servletContext);
+        registerHiddenHttpMethodFilter(servletContext);
+        registerCharacterEncodingFilter(servletContext);
+    }
+
+    private void registerHiddenHttpMethodFilter(ServletContext servletContext) {
+        servletContext.addFilter("hiddenHttpMethodFilter", new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null, true, "/*");
+    }
+
+    private void registerCharacterEncodingFilter(ServletContext servletContext) {
+        EnumSet<DispatcherType> dispatcherTypes = EnumSet.of(DispatcherType.REQUEST, DispatcherType.FORWARD);
+
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+
+        FilterRegistration.Dynamic characterEncoding = servletContext.addFilter("characterEncoding", characterEncodingFilter);
+        characterEncoding.addMappingForUrlPatterns(dispatcherTypes, true, "/*");
+    }
+}
